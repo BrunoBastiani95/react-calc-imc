b5d074e5ce51cd8a3cf5bfd1ae017be96950f9d2
BrunoBastiani95 <brunobastiani95@gmail.com>
2022-03-09 21:27:21 -0300

    finalizado projeto de calculadora imc responsiva

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-Projeto destinado à prática de React.js
+# Calculadora de IMC
 
-Calculadora IMC.
+Projeto realizado em React + Typescript + CSS Modules.
+Projeto desenvolvido durante curso da plataforma [B7Web](https://b7web.com.br).
+
+### Instalação
+- `npm install`
+
+### Para rodar
+- `npm start`
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
 import { GridItem } from './components/GridItem';
 
 import { levels, calculateImc, Level } from './helpers/imc';
@@ -17,7 +18,12 @@ const App = () => {
     else{
       alert("Digite todos os campos.");
     }
+  }
 
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
   }
 
   return (
@@ -39,14 +45,16 @@ const App = () => {
             placeholder="Digite a sua altura. Ex: 1.5 (em métros)"
             value={heightField > 0 ? heightField : ''}
             onChange={e => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
           <input 
             type="number"
             placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
             value={weightField > 0 ? weightField : ''}
             onChange={e => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button disabled={toShow ? true : false} onClick={handleCalculateButton}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
           {!toShow &&
@@ -58,7 +66,9 @@ const App = () => {
           }
           {toShow &&
             <div className={styles.rightBig}>
-              <div className={styles.rightArrow}></div>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25}/>
+              </div>
               <GridItem item={toShow}/>
             </div>
           }
